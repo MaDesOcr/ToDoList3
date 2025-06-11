1f9559d44fb6a6d100f601ca96d08f3130a6b9a8
MaDes <madesappli@gmail.com>
2025-06-11 10:39:22 +0200

    ajout card note (delete a finir)

--- app/src/main/java/com/dev/todolist3/ui/ListeNotesScreen.kt
+++ app/src/main/java/com/dev/todolist3/ui/ListeNotesScreen.kt
@@ -1,5 +1,6 @@
 package com.dev.todolist3.ui
 
+import android.provider.ContactsContract
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,14 +9,21 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.dev.todolist3.data.NoteApi
+import com.dev.todolist3.data.NoteModel
 import kotlinx.serialization.InternalSerializationApi
 
 @OptIn(ExperimentalMaterial3Api::class, InternalSerializationApi::class)
 @Composable
 fun ListeNotesScreen(clickAddNotes: () -> Unit) {
+
+    var listeNotes by remember { mutableStateOf<List<NoteModel>>(emptyList()) }
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -23,15 +31,26 @@ fun ListeNotesScreen(clickAddNotes: () -> Unit) {
     ) {
 
         LaunchedEffect(Unit) {
-            val textapi = try {
+            listeNotes = try {
                 NoteApi.getNotes()
             } catch (e: Exception) {
                 "Erreur lors de la récupération."
-            }
+            } as List<NoteModel>
         }
 
         Column {
             Text(text = "Liste Notes")
+
+            listeNotes.forEach { note -> NoteUi(
+                note,
+                deleteNote = { -> print(note)
+                    /*listeNotes = listeNotes.filter {
+                                    it.id != note.id
+                                }*/
+                } as () -> Unit
+                )
+            }
+
             Button(onClick = clickAddNotes) {
                 Text("Créer une note")
             }
---
+++ app/src/main/java/com/dev/todolist3/ui/NoteUi.kt
@@ -0,0 +1,31 @@
+package com.dev.todolist3.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import com.dev.todolist3.data.NoteModel
+import kotlinx.serialization.InternalSerializationApi
+
+@OptIn(InternalSerializationApi::class)
+@Composable
+fun NoteUi(note : NoteModel, deleteNote : ()-> Unit){
+    //text, date, croix delete
+    Box(modifier = Modifier.fillMaxWidth()) {
+        Row (modifier = Modifier.fillMaxWidth()){
+            Text(text = note.content)
+            Text(text = note.date)
+            IconButton(onClick = deleteNote) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
+            }
+        }
+    }
+}
